38a7c23ae5b5d704686ef8f2bd5c6a446563079b
danielbee <dbee.junk@gmail.com>
2020-08-09 19:08:13 +0100

    Downloaded and created data structure for data.
    haven't added long and lat columns yet

--- scripts/get_weather.py
+++ scripts/get_weather.py
@@ -1,2 +1,20 @@
 # The purpose of this script is to simply download weather data from all the stations listed in stations.txt
 
+import requests
+import os
+
+dataPath = '../data/weather/'
+with open(dataPath+'stations.txt') as f:
+    stations = f.read().splitlines()
+
+dataStationPath = dataPath+'stations/'
+os.makedirs(dataStationPath, exist_ok=True)
+for station in stations: 
+    req = requests.get('https://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/'+station+'data.txt')
+    # Just write the file directly without modification.
+    open(dataStationPath+station+'.txt', 'w').write(req.text)
+
+#print(stationData)
+
+#print(station.text)
+
--- scripts/parse_weather.py
+++ scripts/parse_weather.py
@@ -3,3 +3,74 @@
 
 #the hope is that we can simply export this single data structure to a single file is whatever format we want. 
 
+# Need to figure out how to deal with 'null' values. 
+import re
+
+import pandas as pd
+
+def main(): 
+
+    dataPath = '../data/weather/'
+    dataStationPath = dataPath+'stations/'
+    with open(dataPath+'stations.txt') as f:
+        stations = f.read().splitlines()
+    bigFrame = []
+    stationDataRaw = {}
+    for station in stations: 
+        stationDataRaw[station]= open(dataStationPath+station+'.txt').read().splitlines()
+
+        stationFrame = getDataFrame(stationDataRaw[station])
+        # add a column for the station
+        stationFrame['station'] = station
+        # Make station column the most signifiant index in the multiIndex
+        stationFrame.set_index(['station', stationFrame.index],inplace=True)
+        # Append to list of dataframes
+        bigFrame.append(stationFrame)
+    # Combine all the dataframes
+    stationsData = pd.concat(bigFrame)
+    #print(stationsData)
+    # Print out in desired formats
+    stationsData.to_excel(dataPath+'stationData.xlsx')
+    stationsData.to_csv(dataPath+'stationData.csv')
+
+# bit of an assumption
+tableStart = re.compile('\s{3}yyyy')
+reWord = re.compile('\w+')
+reNum = re.compile('[0-9.]+')
+def getDataFrame(raw):
+    for ln,line in enumerate(raw):
+        if re.search(tableStart,line):
+            tableStartLine = ln
+            # stop going through lines
+            break
+
+    table = raw[tableStartLine:]
+    # remove empty string lines
+    table = list(filter(None, table))
+    headers= table[0].split()
+    #print(headers)
+    prevEnd = 0
+    units = {}
+    headerCols = [re.search(header,table[0]) for header in headers]
+    for colI,col in enumerate(headerCols):
+        units[headers[colI]] = reWord.findall(table[1],prevEnd,col.end())
+        prevEnd = col.end()
+    records = []
+    for row in table[2:]:
+        
+        prevEnd = 0
+        record = {}
+        for colI,col in enumerate(headerCols):
+            res= reNum.findall(row,prevEnd,col.end())
+            
+            record[headers[colI]] = res[0] if res else None
+            prevEnd = col.end()
+        records.append(record)
+    
+    df = pd.DataFrame.from_dict(records)
+    df.set_index(['yyyy', 'mm'],inplace=True)
+    #print(df)
+    return df
+
+
+main()
